d5eb47d90cd2361d33e1a0a9e72e86cec5e71514
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-22 14:12:41 +0300

    Add files via upload

---
+++ javascript/menu.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    
+    var menuLink = ' <li><a href="https://grcodeclub.github.io/programming/main.html">Προγραμματισμός</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/post.html">Έγγραφα </a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/post/colab.html">Google Colab</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/c.html">C</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/c++.html">C++</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/java.html">Java</a></li>';
+     $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/python.html">Python</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/website/main.html">Website</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/docker.html">Docker</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/markdown.html">Markdown</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    
+  });
+  
