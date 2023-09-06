f41b59707c08d949ca085b8e6933c5ef54e78fd2
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-09-06 17:13:34 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     
-    var menuLink = ' <li><a href="https://grcodeclub.github.io/programming/">Προγραμματισμός</a></li>';
+    var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = ' <li><a href="https://grcodeclub.github.io/programming/">Προγραμματισμός</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
     menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
